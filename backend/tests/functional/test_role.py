ea52ac19b9d29c4bbe007a4401008120c2f46816
juanjaho <juansebastianjahowidjaja@gmail.com>
2022-10-06 23:01:56 +0800

    renamed role test data

--- backend/tests/functional/test_role.py
+++ backend/tests/functional/test_role.py
@@ -1,21 +1,10 @@
 from models.RoleModel import Role
+from schemas.RoleSchema import RoleSchema
 from util import post_testing, get_all_testing, get_id_testing
-
 import pytest
 
 
-
-test_data={
-        "name": "TestSkill1",
-        "description": "TestSkill1",
-        "active": True
-        } 
-
-
-
 #create a class fixture for testing roles api
-
-
 class TestRole:
 
     #add fixture to add roles to db
@@ -23,9 +12,11 @@ class TestRole:
     def add_roles(self,db_setup,app):
         self.url="/api/roles/"
         db = db_setup
-        self.role1 = Role(name="TestSkill1",description="TestDesc",active=True)
-        self.role2 = Role(name="TestSkill2",description="TestDesc",active=True)
-        self.role3 = Role(name="TestSkill3",description="TestDesc",active=True)
+        self.app = app
+        
+        self.role1 = Role(name="TestRole1",description="TestDesc",active=True)
+        self.role2 = Role(name="TestRole2",description="TestDesc",active=True)
+        self.role3 = Role(name="TestRole3",description="TestDesc",active=True)
 
         with app.app_context():
             db.session.add(self.role1)
@@ -43,10 +34,17 @@ class TestRole:
         
     #test get role by id
     def test_get_role_by_id(self,client):
-        get_id_testing(client,self.url+"1","name","TestSkill1")
-        # response = client.get(url)
-        # assert response.status_code==200,"response is "+str(response.status_code)
-        # assert response["1"] == value
+        # get_id_testing(client,self.url+"1","name","TestRole1")
+        response = client.get(self.url+"1")
+        assert response.status_code==200,"response is "+str(response.status_code)
+        res = response.json
+        assert res["name"] == "TestRole1"
+        assert res["description"] == "TestDesc"
+        assert res["active"] == True
+
+    
+
+    
     
 
     
