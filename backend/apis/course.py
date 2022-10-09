bc5d0161d02f9f5d9a240054b0246ebe200fddc8
Russell Tio <russelltiojh@gmail.com>
2022-10-09 15:34:47 +0800

    Created API to view courses

--- backend/apis/course.py
+++ backend/apis/course.py
@@ -1,3 +1,4 @@
+from telnetlib import STATUS
 from flask import Blueprint, jsonify, request
 from models.shared_model import db
 from models.CourseModel import Course
@@ -7,7 +8,21 @@ course_schema = CourseSchema()
 courses_schema = CourseSchema(many=True)
 
 course_api = Blueprint('course_api', __name__)
+#Get All Courses
+@course_api.route('/', methods=['GET'])
+def get_courses():
+    all_courses = Course.query.all()
+    return courses_schema.jsonify(all_courses)
 
+# @course_api.route('/', methods=['GET'])
+# def get_courses():
+#     all_courses = Course.query.all()
+#     try:
+#         return courses_schema.dump(all_courses),200
+#     except Exception as e:
+#         return jsonify({
+#             "message": "Unable to retrieve data. "+str(e)
+#         }), 500
 
 
 
