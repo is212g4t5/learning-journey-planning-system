c4dda7c93da86eec0f3b16cc94c6cd0c64f1ddef
Greg G Tan <greggtan99@gmail.com>
2022-10-18 23:31:03 +0800

    fixed spacing for cards for view courses

--- frontend/src/pages/ViewCoursesPage.vue
+++ frontend/src/pages/ViewCoursesPage.vue
@@ -14,7 +14,7 @@
       <q-breadcrumbs-el to="">View Courses</q-breadcrumbs-el>
     </q-breadcrumbs>
 
-    <div class="q-mt-md q-px-md" style="background:#EFF8ED;height:90vh">
+    <div class="q-mt-md q-px-md" style="background:#EFF8ED;padding-bottom:140px">
       <div class="text-center" style="color:#007AFF;font-size:50px;font-weight:600">{{currSkillInfo.name}}</div>
 
       <div
@@ -30,31 +30,32 @@
 
 
       <div class="flex items-center justify-around">
-        <div v-for="course in coursesWithSkill" :key="course.id" class="" style="width:350px;background:white;border-radius:5px">
+        <div v-for="course in coursesWithSkill" :key="course.id" class="" style="width:350px;height:550px;background:white;border-radius:5px;position:relative">
           <img src="~assets/courseImage.png" style="width:100%;border-top-left-radius:5px;border-top-right-radius:5px" alt="">
           <div class="q-px-md q-py-sm">
             <div class="q-mb-sm" style="font-size:20px;font-weight:700">{{course.name}}</div>
             <div class="q-mb-sm" style="color:#676767;font-size:16px;font-weight:700">{{course.category}} ({{course.id}})</div>
             <div class="" style="color:#737373; font-size:14px">{{course.description}}</div>
-            <!-- buttons div -->
-            <div class="flex items-center q-gutter-x-sm q-mt-md">
 
+           <div class="" style="position:absolute;bottom:20px">
+              <!-- buttons div -->
+              <div class="flex items-center q-gutter-x-sm q-mt-md">
 
+                <div v-for="skill in course.skills" :key="skill.id" class="" style="">
+                  <div v-if="skill.id == currSkillId" class="q-pa-sm" style="background:#96BB7C; border-radius:10px">{{skill.name}}</div>
+                  <div v-else class="q-pa-sm" style="background:#E6E6E6; border-radius:10px">{{skill.name}}</div>
 
-              <div v-for="skill in course.skills" :key="skill.id" class="" style="">
-                <div v-if="skill.id == currSkillId" class="q-pa-sm" style="background:#96BB7C; border-radius:10px">{{skill.name}}</div>
-                <div v-else class="q-pa-sm" style="background:#E6E6E6; border-radius:10px">{{skill.name}}</div>
-              
-              </div>
+                </div>
+                </div>
 
-              
+                <div class="q-mt-md">
+                <q-icon name="place" size="sm" style="color:#96BB7C"></q-icon>
+                {{course.type}}
+                </div>
+           </div>
+          
 
-            </div>
-
-            <div class="q-mt-md">
-              <q-icon name="place" size="sm" style="color:#96BB7C"></q-icon>
-              {{course.type}}
-            </div>
+           
           </div>
           
         </div>
