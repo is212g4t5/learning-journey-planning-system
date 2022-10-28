3f7d6ee4f0a33d8941c7cbd83daa4ed993d31bc7
juanjaho <juansebastianjahowidjaja@gmail.com>
2022-10-28 12:34:38 +0800

    lj delete api and eligible courses api test

--- backend/tests/integration/test_lj.py
+++ backend/tests/integration/test_lj.py
@@ -1,6 +1,9 @@
 import pytest
 from models.LearningJourneyModel import LearningJourney
 from tests.util import get_all_testing, post_testing
+from models.SkillModel import Skill
+from models.RoleModel import Role
+from models.CourseModel import Course
 
 
 class TestLJ:
@@ -10,6 +13,7 @@ class TestLJ:
         self.db = db_setup
         self.app = app
 
+    #test create LJ and invalide create LJ (duplicate name)
     def test_post_LJ(self,client,create_courses):
         test_data={
             "name":"NewLJ",
@@ -19,6 +23,7 @@ class TestLJ:
         }
         post_testing(client,self.url+"create",test_data,"name","NewLJ")
 
+    #test get all LJ
     def test_get_all_LJ(self,client, create_ljs):
         get_all_testing(client,self.url,2)
 
@@ -35,6 +40,63 @@ class TestLJ:
             assert res["name"] == lj.name
             assert res["role_id"] == lj.role_id
             assert len(res["courses"]) == 2
+
+    #test view courses in LJ
+
+    #test view courses in LJ that has yet to be added to LJ
+    def test_get_eligible_courses_not_in_LJ(self,client, create_ljs):
+        
+        with self.app.app_context():
+            #assign skills to courses
+            course1 = Course.query.get("COR001")
+            course2 = Course.query.get("COR002")
+            course3 = Course.query.get("COR003")
+            course1.skills = [Skill.query.get(1),Skill.query.get(2),Skill.query.get(3)]
+            course2.skills = [Skill.query.get(1),Skill.query.get(2),Skill.query.get(3)]
+            course3.skills = [Skill.query.get(1),Skill.query.get(2),Skill.query.get(3)]
+
+            #assign skills to role
+            role = Role.query.get(1)
+            role.skills = [Skill.query.get(1),Skill.query.get(2),Skill.query.get(3)]
+
+            #assign role to lj
+            lj = LearningJourney.query.get(1)
+            lj.role = role
+            
+            #assign courses to lj
+            lj.courses = [course1,course2]
+
+            self.db.session.commit()
+
+            #test get eligible courses
+            response = client.get(self.url+"1?courses_not_in_lj=true")
+            assert response.status_code==200
+            res = response.json
+            assert len(res) == 1
+            assert res[0]["id"] == "COR003"
+
+
+
+            
+    #test delete LJ
+    def test_delete_LJ(self,client, create_ljs):
+        response = client.delete(self.url+"2")
+        assert response.status_code==200
+        res = response.json
+        assert res["message"] == "Learning Journey deleted"
+
+        with self.app.app_context():
+            lj = LearningJourney.query.get(2)
+            assert lj == None
+
+    #test delete LJ that does not exist
+    def test_delete_LJ_not_exist(self,client, create_ljs):
+        response = client.delete(self.url+"4")
+        assert response.status_code==404
+        res = response.json
+        assert res["message"] == "Learning Journey does not exist"
+        
+
 
 
 
